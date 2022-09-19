56b3eab802208de355fb7bf4ee24abb625d41dc2
Jakub Cieślik <kuba8172@gmail.com>
2022-09-19 16:31:59 +0200

    Work3

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
-const express = require("express");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import productRoute from "./routes/productRoute.js";
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -7,13 +13,10 @@ app.get("/", (req, res) => {
   res.send("API is running.");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoute);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
-
-app.listen(5000, console.log("server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
